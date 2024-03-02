708f48d968dae4d63dc6ae7322a8d54c3748b10c
Callum Dempsey Leach <callum.leach@hotmail.co.uk>
2024-03-02 11:44:43 +0000

    refinement: get_spark_deps.sh does not need to be a function

--- infra-scripts/get_spark_deps.sh
+++ infra-scripts/get_spark_deps.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-download_dependency() {
-    rm -rf infra-data-lake/spark/_build
-    mvn_command="mvn -f infra-data-lake/poms/spark.pom dependency:copy-dependencies -DoutputDirectory=../spark/_build/spark_jars -Dscope=provided"
+rm -rf infra-data-lake/spark/_build
+mvn_command="mvn -f infra-data-lake/poms/spark.pom dependency:copy-dependencies -DoutputDirectory=../spark/_build/spark_jars -Dscope=provided"
 
-    # Execute Maven command
-    if ! $mvn_command; then
-        echo "❌ Error occurred while downloading Spark dependencies."
-        exit 1
-    else
-        echo "✅ Spark dependencies download completed successfully."
-    fi
-}
-
-download_dependency 
+# Execute Maven command
+if ! $mvn_command; then
+    echo "❌ Error occurred while downloading Spark dependencies."
+    exit 1
+else
+    echo "✅ Spark dependencies download completed successfully."
+fi
